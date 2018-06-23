ff48512a262967f1500e8ab4022e7bc5b4414ff6
Lucy <lucy.silulyu@gmail.com>
2018-06-22 22:53:55 -0700

    Leetcode problem 1 solution
    * lc twice
    
    * Address feedback update 1
    
    * Address feedback update 2
    
    * Address feedback update 3
    
    * Address feedback update 4

---
+++ L1_Two_Sum.py
@@ -0,0 +1,41 @@
+# Input 
+# List of numbers, target as an integer
+
+# Output 
+# List of indexes whose corresponding integers sum up to the target
+
+# Solution
+# To solve the problem, apply data structure "dictionary" 
+# to store integer and its index as a key-value pair.
+# During the list iteration, we check if target - current number is already in the dict.
+# If it is, we find the answer.
+# If not, we store the current number and index in the dict for thr next round checking.
+
+# Clarification
+# Can I assume there is a unique combination of two integers or multiple solution?
+# In that case, what should we do? 
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        # First, check the boundary cases
+        res = []
+        if not nums:
+            return res
+        
+        dict = {} 
+        
+        for i in range(len(nums)): # Iterate over the list of given nums
+            if target - nums[i] in dict:
+                res.append(dict[target - nums[i]])
+                res.append(i)
+                return res     
+            dict[nums[i]] = i # If target - current number is not in the dict, we store key value pair for further check 
+
+# Time complexity: O(n)
+# Space complexity: O(n)
