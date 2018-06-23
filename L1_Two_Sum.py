1dbab658788ddca3dfd593413cd549d49016bbf5
SILU LYU <lucy.silulyu@gmail.com>
2018-06-22 22:05:28 -0700

    Address feedback update 2

--- L1_Two_Sum.py
+++ L1_Two_Sum.py
@@ -1,8 +1,8 @@
-# Input is a list of numbers and a target; 
-# Output should a list of indexes for two integers whose sum is the target
-# The data structure I use is hashmap
+# Input is a list of numbers and a target
+# Output should be a list of indexes for two integers whose sum is the target
+# The data structure I use is dictionary
 # store the value of the integer in "key" 
-# and coresponding index in "value"
+# and corresponding index in "value"
 
 class Solution(object):
     def twoSum(self, nums, target):
@@ -11,20 +11,24 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+
+
         # First, check the boundary cases
         res = []
         if not nums:
             return res
         
-        # Creat a hashmap to store key value pair
+        # Creat a dictionary to store key value pair
         dict = {}
+
         # Interate over the list of given nums
-        for i in range(len(nums)): # Check before updating the hashmap
-            if target - nums[i] in dict: 
+        for i in range(len(nums)): 
+            if target - nums[i] in dict: # Check if the difference
+            # between the target and current number is in the dictionary
                 res.append(dict[target - nums[i]])
                 res.append(i)     
-            dict[nums[i]] = i # Store key value pair into the hashmap
+            dict[nums[i]] = i # Store key value pair for further check 
         return res
 
-#Time complexity: O(n)
-#Space complexity: O(n)
+# Time complexity: O(n)
+# Space complexity: O(n)
