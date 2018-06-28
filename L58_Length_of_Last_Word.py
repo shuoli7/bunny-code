36d36b2d96fd0332b5752008ab662fa710fe9c4c
SILU LYU <lucy.silulyu@gmail.com>
2018-06-27 19:36:35 -0700

    Leetcode 58 update 1

--- L58_Length_of_Last_Word.py
+++ L58_Length_of_Last_Word.py
@@ -1,4 +1,4 @@
-# Input: A string of words seperated by " "
+# Input: A string of words seperated by space
 # Output: The length of the last word
 
 # Solution: 
@@ -11,10 +11,10 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        s = s.split()
+        s = s.split() # splits string with space by default
         if not s:
             return 0
         return len(s[-1])
 
-# Time complexity: O(1)
+# Time complexity: O(n)
 # Space complexity: O(1)
