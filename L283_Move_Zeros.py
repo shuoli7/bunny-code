ef6d0da639972f7c4c6bd52731c1510ca9de77aa
SILU LYU <lucy.silulyu@gmail.com>
2018-06-27 22:02:10 -0700

    Leetcode 283 update 1

--- L283_Move_Zeros.py
+++ L283_Move_Zeros.py
@@ -2,12 +2,22 @@
 # A list of numbers
 
 # Output
-# Not return anything
+# No output, because this is a void function.
 
 # Solutions
-# Use two points, one point points to the leftmost "0", the other point points to the current number. 
-# Iterate over the list. 
-# If the current number is not 0, make in-place change on "0" and the current numbers.
+# To solve this problem, we will set two pointers.
+
+# First pointer i, we use it to iterate through the list.
+# Second pointer j, we place it in a position that all the positions left to it are non-zeros.
+# We start j = 0.
+
+# During the iteration, if the current number is non-zero, we swap the number with the number pointed by j.
+# Then we increment j. 
+# In this way, we ensure that non-zero numbers are all left to position j and their orders
+# are not changing.
+
+# At the end, we reach a situation that all positions left to j are non-zero, right to j are zeros or maybe nothing.
+# Note that, the number at position j could be zero or non-zero, doesn't matter.
 
 class Solution(object):
     def moveZeroes(self, nums):
