0a3702003b232e65c3f7ce0c2ca9a360ecf3a23a
SILU LYU <lucy.silulyu@gmail.com>
2018-06-27 22:14:26 -0700

    Leetcode 14 update 4

--- L14_Longest_Common_Prefix.py
+++ L14_Longest_Common_Prefix.py
@@ -25,14 +25,11 @@ class Solution(object):
         if not strs:
             return ""
         
-        res = ""
-        
         for i in range(len(strs[0])):
             for j in range(1, len(strs)):
                 if i >= len(strs[j]) or strs[j][i] != strs[0][i]:
-                    return res
-            res = strs[0][:i + 1]
-        return res
+                    return strs[0][:i]
+        return strs[0]
 
 # Time complexity: O(mn) # m is length of first string, n is length of the list of strings.
 # Space complexity: O(1)
