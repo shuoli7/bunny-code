6515e23a994d3ddf2fc6e4acabef7b57ef2115ff
SILU LYU <lucy.silulyu@gmail.com>
2018-07-05 22:42:45 -0700

    Leetcode 121 update 1

--- L121_Best_Time_to_Buy_and_Sell_Stock.py
+++ L121_Best_Time_to_Buy_and_Sell_Stock.py
@@ -1,23 +1,23 @@
 # Input:
-# A list of stock price on one day
+# A list of stock prices, each price is for one day
 
 # Output:
-# The maximum profit we can get if at most one transaction permitted.
+# An integer, which represents the maximum profit we can make if at most one transaction permitted.
 
 # Solution:
-# Two solve this problem, we have to update the minumum price and maximum profit during the iteration of the list.
-# As we have to purchase stock before sell it, we set the first value as the minimum price first.
-# And the starting maximum profit is the fist value minus minimum, which is 0.
+# To solve this problem, we have to update the minumum price and maximum profit during the iteration of the list.
+# As we have to purchase stock before sell it, we set the first price as the minimum price first.
+# And the starting maximum profit is the fist price minus minimum, which is 0.
 
 # Then, we iterate over the list. 
 
-# If the current price is less than the minumum, we update the minimum.
+# we compare the current minumum price with the current price, and update the current minimum price.
 
-# If the current price is larger than the minumum, we substract the current price by the minumum
-# and compare it with the current maximum.
+# Then, we substract the current price by the current minumum price
+# and compare it with the current maximum profit.
 
-# If the caculated maximum is greater than the maximum, then we update it.
-# Or we will keep the previous maximum.
+# If the caculated maximum profit is greater than the current maximum profit, then we update it.
+# Or we will keep the previous maximum profit.
 
 # At the end of iteration, we output the maximum profit.
 
@@ -27,16 +27,15 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        #Edge case: if the input list is empty or all to be zero, the result is 0.
         if not prices:
             return 0
         
-        minimum = prices[0]
-        maximum = 0
+        currMin = prices[0]
+        max_profit = 0
         for price in prices:
-            minimum = min(minimum, price)
-            maximum = max(price - minimum, maximum)
-        return maximum
+            currMin = min(currMin, price)
+            max_profit = max(price - currMin, max_profit)
+        return max_profit
 
 # Time complexity: O(n)
 # Space complexity: O(1)
