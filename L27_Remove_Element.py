104b4b901c728fe101e7b6301a3d27fda1cfdb39
SILU LYU <lucy.silulyu@gmail.com>
2018-07-06 21:33:04 -0700

    Leetcode 27 update 1

--- L27_Remove_Element.py
+++ L27_Remove_Element.py
@@ -2,7 +2,7 @@
 # A list of numbers and a value.
 
 # Output
-# The length of new list after deleting all numbers of the input value from the list.
+# Integer, which represents the length of new list after deleting all numbers of the input value.
 
 # Solutions
 # To solve this problem, we will set two pointers.
@@ -20,7 +20,7 @@
 
 # At the end, we reach a situation that all positions left to j are not same as the input value, 
 # right to j are same as the input value or maybe nothing.
-# Then We get the sliced list up to position j.
+# Then We get the sliced list up to position j and return the length of sliced list.
 
 class Solution(object):
     def removeElement(self, nums, val):
