e4976a6a1837121f8d5644290e9242e514d0d973
SILU LYU <lucy.silulyu@gmail.com>
2018-07-06 22:57:13 -0700

    Leetcode 242 update 1

--- L242_Valid_Anagram.py
+++ L242_Valid_Anagram.py
@@ -1,6 +1,8 @@
-# Input: Two strings	
+# Input:
+# Two strings	
 	
-# Output: If one string is an anagram of another string, return True; 	
+# Output:
+# If one string is an anagram of another string, return True; 	
 # Otherwise, return False.	
 	
 # Solution 1:	
@@ -21,11 +23,15 @@ class Solution(object):
 # Space complexity: O(1)	
 	
 # Solution 2:	
-# Create a unicode list with the length of 26.	
-# Loop over the first string, count the frenquency of each character	
-# and put it at the location based on the character's unicode.	
-# Loop over the second string, subtract the frequency of each character.	
-# If the list ends with all zeros, return True; Else, return False.	
+# First, we create a unicode distance list with length of 26.
+# The index here is the unicode distance from current char to 'a'.
+# The value at each index is the count of appearance of each character.
+
+# Then we iterate the first string, increment the value at the unicode distance location by 1.
+
+# Next, we iterate the second string, decrement the value at the unicode distance location by 1.
+
+# If the list ends with zeros at all position, return True; Else, return False.	
 	
 class Solution(object):	
     def isAnagram(self, s, t):	
@@ -36,25 +42,29 @@ class Solution(object):
         """	
         char = [0] * 26
         
-        # Add the frenquency of characters in the s string to the list
+        # Increment the frequency of this char by 1
         for c in s:	
             char[ord(c) - ord("a")] += 1
         
-        # Substract the frenquency of characters in the t string from the list
+        # Decrement the frequency of this char by 1
         for c in t:	
             char[ord(c) - ord("a")] -= 1
             
-        # If t is the anagram of t, then all the numbers in the list should be zero. 
+        # If t is the anagram of s, then all the numbers in the list should be zero. 
         for i in range(26):	
-            if char[i]!= 0:	
+            if char[i] != 0:	
                 return False	
         return True	
 	
 # Time complexity: O(n)	
-# Space complexity: O(n)	
+# Space complexity: O(1) # if all the char in given string is small-case alphabetical char.
+                         # If it is, then the helper list has a constant length of 26, 
+                         # it is not really growing with n.
 
 # Solution 3:
 # if the inputs contain unicode characters, I use a dictionary instead
+# because there won't be a fixed size of my helper data structure.
+# Add or update info to dict is an O(1) action.
 
 class Solution(object):
     def isAnagram(self, s, t):
@@ -72,15 +82,18 @@ class Solution(object):
             else:
                 dict[c] += 1
                 
-        # Substract the frenquency of characters in the t string from the list 
+        # Iterate over t string, subtract frequency of char from the dict. 
         for c in t:
             if c not in dict:
                 return False
             else:
                 dict[c] -= 1
         
-        # If t is the anagram of t, then all the values in the dictionary should be zero. 
+        # If t is the anagram of s, then all the values in the dictionary should be zero. 
         for value in dict.values():
-            if value!= 0:
+            if value != 0:
                 return False
         return True
+
+# Time complexity: O(n) 
+# Space complexity: O(n)
