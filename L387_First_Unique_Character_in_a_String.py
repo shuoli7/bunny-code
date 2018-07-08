f39b6dc37db24aac627ae4aafdc51492b2cf10a1
SILU LYU <lucy.silulyu@gmail.com>
2018-07-07 20:40:23 -0700

    Leetcode 387 update 1

--- L387_First_Unique_Character_in_a_String.py
+++ L387_First_Unique_Character_in_a_String.py
@@ -1,35 +1,19 @@
-# Input: A string
-# Output: The first non-repeating character in the string and return it's index. 
+# Input:
+# A string
 
-# Solution1 (Brute force)-- Time Limit Exceeded 
-# Compare each character with followed character in the string to see whether we can find the non-repeating character.
-# As soon as we find it, return the index of the character
-# If we can not find such character after the iteration over the string, return -1
-
-# Time complexity: O(n^2)
-# Space complexity: O(1)
-
-class Solution(object):
-    def firstUniqChar(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        i = 0
-        for i in range(len(s)):
-            isExist = True
-            for j in range(len(s)):
-                if i != j and s[i] == s[j]:
-                    isExist = False
-                    break
-            if isExist:
-                return i   
-        return -1
+# Output:
+# An integer, which represents the index of first non-repeating char in the string.
 
 # Solution2 (hashmap)
-# Create a hashmap with counts
-# Loop over the string and check for counts of each character
-# if count is 1, then return index; else return -1
+# To solve this problem, we use a dictionary to help implementation.
+
+# Loop over the string,
+# I will set each character as the key and its frequency of appearance as value.
+
+# After iteration, I loop over string again using index.
+# In each round, I will check if the value for that character in dict is 1.
+# If value is 1, we return that index.
+# If at the end we still didn't find a key with value 1 in dict, we return -1.
 
 # Time complexity: O(n)
 # Space complexity: O(n)
@@ -40,26 +24,38 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        d = {}
-        for c in s:
-            if c in d.keys():
-                d[c] += 1
-            d[c] = 1
+        dic = {}
+        for char in s:
+            if char in dic.keys():
+                dic[char] += 1
+            else：
+                dic[char] = 1
         
         for i in range(len(s)):
-            c = s[i]
-            if d[c] == 1:
+            char = s[i]
+            if dic[char] == 1:
                 return i
         return -1
 
 # Solution3 (unicode)
-# Creat a list with the length of 26, representing 26 positions for 26 positions
-# Loop over the string, add 1 to its corresponding location if the character shows up
-# Loop over the string again, if the character only showed up once, return its index
-# If all characters show up more than once, return -1
+# To solve this problem, we create a list with length of 26.
+
+# The index of this list represents the unicode distance from current char to 'a',
+# The value of this list represents the frequency of appearance at each position.
+
+# Loop over the string, compute the unicode distance, 
+# and increment the value at that position by 1.
+
+# Loop over the string again, if we find value is 1 at certain position, 
+# we return the index immediately.
+
+# If at the end, we don't find a position with value 1. We return -1.
 
 # Time complexity: O(n)
-# Space complexity: O(n)
+# Space complexity: O(1) # If only small-case alphabetical letters are given, 
+                         # then we know the size of this fixed sized list is 26.
+                         # The length of this list won't increase with
+                         # the length of given string. So we can think space is O(1).
 
 class Solution(object):
     def firstUniqChar(self, s):
@@ -67,7 +63,7 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        chars = [0]*26
+        chars = [0] * 26
     
         for i in range(len(s)):
             chars[ord(s[i]) - ord('a')] += 1
