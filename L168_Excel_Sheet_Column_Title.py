2fc733372cf94f9463ddbe3b274062693d9d08ff
SILU LYU <lucy.silulyu@gmail.com>
2018-07-09 23:29:00 -0700

    Leetcode 168 update 1

--- L168_Excel_Sheet_Column_Title.py
+++ L168_Excel_Sheet_Column_Title.py
@@ -1,17 +1,26 @@
-# Input: A number
+# Input:
+# An integer number
 
-# Output: A string of letters, which represents corresponding column title in Excel
+# Output:
+# A string of letters, which represents corresponding column title in Excel
 
-# Solution: Basically, it is to transform Decimal system to Hexadecimal.
+# Solution:
+# To solve this problem, we run a while loop, the loop continue condition is n > 0.
+# Generally, we want to find the string of letters from right to left.
 
-# First, we get the reminder of (the given number - 1) divided by 26.
-# Then, the reminder is the distance between A and the right most letter in the output.
-# We can transform the reminder to a letter by built-in Python function, "Chr".
+# In each round, first, we compute the the distance between right most letter
+# and 'A' by take mod 26 on n - 1.
 
-# Then, we divided the number by 26, and follow the above steps again.
-# Everytime we get a new letter, we put it in the left of the current result.
-# We stop until n is zero. At this moment, we get the string of letters
-# which represents the input number.
+# The reason we do n - 1 is 'A' equals to 1; For example, AB -> 28,
+# with our logic, 28 % 26 = 2, we get C, so
+# we should compensate that by minus 1 first.
+
+# Then, we can get the letter by using build-in python function chr().
+
+# We add this letter to the left-most position to previous result,
+# then we divide n by 26, and go to the next round.
+
+# After finishing all rounds, we have the string of letters.
 
 class Solution(object):
     def convertToTitle(self, n):
@@ -22,11 +31,11 @@ class Solution(object):
         res = ""
         
         while n > 0:
-            n -= 1
-            unit = n % 26
+            n -= 1 # right-shift n by 1 step
+            unit = n % 26 # get right-most letter
             res = chr(ord("A") + unit) + res
             n /= 26
         return res
             
 # Time complexity: O(n)
-# Space complexity: O(n)
+# Space complexity: O(1)
