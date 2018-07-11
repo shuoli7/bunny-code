96fd0d47e14b93be5d1e7c833b4bc3ee11a9638e
SILU LYU <lucy.silulyu@gmail.com>
2018-07-10 22:25:18 -0700

    Leetcode 26 update 2

--- L26_Remove_Duplicates_from_Sorted_Array.py
+++ L26_Remove_Duplicates_from_Sorted_Array.py
@@ -1,6 +1,8 @@
-# Input: A sorted list of numbers
+# Input:
+# A sorted list of numbers
 
-# Output: The length of the list without duplicate numbers
+# Output:
+# An integer, which represents the length of the list without duplicate numbers.
 
 # Solution:
 # To solve theis problem, we will set two pointers.
@@ -8,13 +10,17 @@
 # First pointer i, we use it to iterate through the list.
 # Secont pointer j, we use it to count how many distinct numbers in the list.
 
-# During the iteration, if the current number is different from the previous one,
-# set the number whose index is the current count to be this distinct number,
-# and increase the count index by one.
-# In this way we put all distinct numbers in order.
+# Initialize j with 1
+# Because if the list is not empty, we should at least have 1 distinct number.
 
-# At the end, the list without duplicate numbers is the sliced list end at the lndex of count
-# we return the length of the sliced list
+# During the iteration, if the current number is different from the previous one,
+# set number at position with index j to be this distinct number,
+# and increase the count index j by 1.
+# In this way we can make sure that all the numbers left to position j are distinct.
+
+# At the end, we can simply return j.
+# As all the numbers left to position j are distinct,
+# the length of the list without duplicate numbers is j.
 
 class Solution(object):
     def removeDuplicates(self, nums):
@@ -22,16 +28,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # Edge case: if the list did not exist or the length is 0, then we return 0.
+        # Edge case: check if the list is empty
         if not nums:
             return 0
         
-        j = 1 # There are at least one distinct number in the list
+        j = 1 # There are at least 1 distinct number in the list
         for i in range(len(nums) - 1):
             if nums[i + 1] != nums[i]:
                 nums[j] = nums[i + 1]
                 j += 1
-        return len(nums[:j]) # or return j
+        return j
 
 # Time complexity: O(n)
 # Space complexity: O(1)
