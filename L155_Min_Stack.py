ca93ae7780bdc84e527e9f924c5238d4413749b1
SILU LYU <lucy.silulyu@gmail.com>
2018-07-15 20:57:20 -0700

    Leetcode 155 update 1

--- L155_Min_Stack.py
+++ L155_Min_Stack.py
@@ -1,9 +1,26 @@
 # In this problem, we need to design a stack that supports push, pop, top,
-# and retrieving the minimum element in constant time.
+# and retrieve the minimum element all in constant time.
 
 # To solve the problem, we need to initiate two stacks.
 # One is used to store elements pushed in;
-# The other one is used to store minimum elements.
+# The other one is used to keep track of min element
+# at each moment we push a new element in first stack.
+
+# push():
+# If the number we add is less or equal to the minimum value in the min_stack,
+# Or if the min_stack is empty,
+# then, we add this number to both min_stack, and stack.
+
+# pop():
+# If the number we pop out is equal to the current minimum value,
+# then, we pop out this number from both min_stack, and stack.
+
+# top():
+# In stack, the top element is the last added element.
+
+# getMin():
+# The minimum element is the last element in the min_stack.
+
 
 class MinStack(object):
 
@@ -19,7 +36,9 @@ class MinStack(object):
         :type x: int
         :rtype: void
         """
-        if len(self.min_stack) == 0 or x <= self.min_stack[-1]:
+        if len(self.min_stack) == 0 or x <= self.min_stack[-1]: # The reason we use <= instead of <:
+                                                                # is because there could be multiple appearances 
+                                                                # of one current min element.
             self.min_stack.append(x)
         self.stack.append(x)
 
