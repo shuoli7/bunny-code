03d4ca3c3cda1c8ccbe28395f04a328665521c8c
SILU LYU <lucy.silulyu@gmail.com>
2018-07-16 22:58:39 -0700

    Leetcode 232 update 4

--- L232_Implement_Queue_using_Stacks.py
+++ L232_Implement_Queue_using_Stacks.py
@@ -4,20 +4,21 @@
 # Time complexity for peek is O(n)
 # Time complexity for empty is O(1)
 
-# To solve this problem, we need to store the elements in order in stack 1.
-# Then we pop elements in stack 1 out and store them in stack 2.
-# In this way, we store elements in a reversed order in stack 2.
+# To solve this problem, we need to store the elements in order in stack 1,
+# and store elements in a reversed order in stack 2.
 
 # When we operate push, we add the new element to stack 1.
 
-# When we operate pop, we recall the peek function to get the front element in stack 1,
-# which is in fact last element in stack 2.
+# When we operate pop, we call the peek function to
+# shift your elements in stack 1 to stack 2 and store them in the reversed order.
 # So we just pop out the last element in stack 2.
 
 # When operating peek, to obtain the top element in stack 1,
 # we actually look for the last element in stack 2
 # because elements in stack 2 are in a reversed order.
-# If stack 2 is empty, we pop out elements in stack 1 and add them into stack 2.
+
+# To store elements in a reversed order in stack 2, we pop out elements in stack 1
+# and add them into stack 2.
 # In this way, the last element in stack 2 is exactly the front element in stack 1.
 
 # When operating empty, we check the length of both stacks.
