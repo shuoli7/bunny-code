3b962a92c2998d2b70d1ec013b9de71c67535e2f
SILU LYU <lucy.silulyu@gmail.com>
2018-07-25 21:48:56 -0700

    Leetcode 226 update 4

--- L226_Invert_Binary_Tree.py
+++ L226_Invert_Binary_Tree.py
@@ -41,3 +41,48 @@ class Solution(object):
 
 # Time complexity: O(n)
 # Space complexity: O(h)
+
+# Solution (BFS)
+
+# We use BFS to solve this problem.
+
+# We initiate queue to be a list with root node.
+# Then, we pop the last element in the queue out to check its children.
+
+# If node.left exists, we add it to the queue.
+# If node.right exists, we add it to the queue.
+
+# Then, we swap node.left and node.right.
+
+# We keep swapping node.left and node.right level by level from up to bottom.
+# At the end, we invert the tree.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        
+        queue = [root]
+        
+        while queue:
+            node = queue.pop(0)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+            node.left, node.right = node.right, node.left
+        return root
+
+# Time complexity: O(n)
+# Space complexity: O(n)
