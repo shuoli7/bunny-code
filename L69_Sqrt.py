eec6c10181575d191ae84daf7daa8c1b96176af7
SILU LYU <lucy.silulyu@gmail.com>
2018-09-19 11:31:35 -0700

    Leetcode 69 Solution

---
+++ L69_Sqrt.py
@@ -0,0 +1,21 @@
+class Solution:
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x == 0:
+            return 0
+        
+        left = 1
+        right = x
+        
+        while left <= right:
+            mid = left + (right - left) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid > x:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return (left + right) // 2
